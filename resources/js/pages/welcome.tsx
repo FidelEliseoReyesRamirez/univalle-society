898f2218359cd4d4c433d7f41a694efb1fbf89ee
Maho Salinas <majitosp2002@gmail.com>
2026-01-24 21:01:56 -0400

    Cabecera de HomePage

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -3,6 +3,8 @@ import { Head, Link, usePage } from '@inertiajs/react';
 import { dashboard, login, register } from '@/routes';
 import { type SharedData } from '@/types';
 
+import AppLogoIconHor from '@/components/app-logo-hor-icon';
+
 export default function Welcome({
     canRegister = true,
 }: {
@@ -20,20 +22,44 @@ export default function Welcome({
                 />
             </Head>
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-                    <nav className="flex items-center justify-end gap-4">
+                <header className="mb-6 w-full text-sm not-has-[nav]:hidden lg:max-w-8xl">
+                    <nav className="flex justify-between items-center gap-4 border-2 border-amber-600">
+                        <div className='flex items-center'>
+                        <div>
+                            <AppLogoIconHor/>
+                        </div>
                         <Link
                             href="#Noticias"
                             className='inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]'
                         >
-                            Noticias
+                            Eventos
                         </Link>
+                        <Link
+                            href="#Noticias"
+                            className='inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]'
+                        >
+                            Promociones
+                        </Link>
+                        <Link
+                            href="#Noticias"
+                            className='inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]'
+                        >
+                            Proyectos
+                        </Link>  
+                        <Link
+                            href="#Noticias"
+                            className='inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]'
+                        >
+                            <b>Contáctanos</b>
+                        </Link>
+                        </div>
+                        <div className='flex'>                                          
                         {auth.user ? (
                             <Link
                                 href={dashboard()}
                                 className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                             >
-                                Dashboard
+                                Contacto
                             </Link>
                         ) : (
                             <>
@@ -53,6 +79,7 @@ export default function Welcome({
                                 )}
                             </>
                         )}
+                        </div>
                     </nav>
                 </header>
 
