7e99c0f9033609f91d46e174384702aa06073ecc
Maho Salinas <majitosp2002@gmail.com>
2026-01-24 20:12:33 -0400

    Agregación de Google Fonts: Roboto and Bebas Neue para tipografía personlizada

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,6 +39,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        <!-- Google Fonts: Roboto (body) + Bebas Neue (display) -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Bebas+Neue&display=swap" rel="stylesheet">
+
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
